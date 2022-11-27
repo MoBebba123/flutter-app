e037d3ea0c2aebbc7620fdebfe693747ff754d54
Mohamed Bebba <mohamedbebba1@gmail.com>
2022-11-27 19:36:46 +0100

    main

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,51 @@ void main() {
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: const Center(
-        child: Text(
-          "Register",
-          textAlign: TextAlign.center,
-        ),
-      )),
-    );
+        appBar: AppBar(
+            title: const Center(
+          child: Text(
+            "Register",
+            textAlign: TextAlign.center,
+          ),
+        )),
+        body: Column(children: <Widget>[
+          const TextField(
+            decoration: InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: "Enter yout e-mail",
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const TextField(
+            decoration: InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: "Enter your password",
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          TextButton(
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.black54)),
+            child: const Text('register '),
+            onPressed: () async {},
+          ),
+        ]));
   }
 }
